3077008a7b44c7dec1f32984dd531919585b14cb
ashwani rathee <ashwanirathee.work@gmail.com>
2023-10-13 00:45:16 +0530

    Checkerboard detection for calibration purposes

--- ext/CalibrateExt.jl
+++ ext/CalibrateExt.jl
@@ -3,24 +3,56 @@ module CalibrateExt
 using GridDetector, VideoIO
 using MultipleViewGeometry
 
-function MultipleViewGeometry.calibrate(cam)
+function MultipleViewGeometry.calibrate(device;save=false)
+    cam = VideoIO.opencamera("video=" * device)
+    
     count = 0
     @info count "Calibrating..."
     fps = VideoIO.framerate(cam)
-    while (count < 1000)
-        try
-            img = Gray.(read(cam))
-            @info "Checkerboard size:" length(process_image(img)) 
-            count = count + 1
-            sleep(0.01)
-        catch e
-            @info e.message
-            if typeof(e) <: InterruptException
-                println("caught Interrupt")
-                return
+    images = []
+    corners = []
+    
+    try
+        while (count < 9)
+            try
+                img = Gray.(read(cam))
+                cam.flush  = true
+
+                # @info "Info:" cam.flush cam.finished cam.bits_per_result_pixel cam.frame_queue
+                res = find_checkerboard(img) 
+
+                if (length(res) == 63)
+                    @info "Checkerboard size:" length(res) 
+                    push!(images, img)
+                    push!(corners, res)
+                    count = count + 1
+                    sleep(0.5)
+                else
+                    sleep(0.01)
+                end  
+
+                cam.flush = false
+            catch e
+                @info e.message
+                if typeof(e) <: InterruptException
+                    println("caught Interrupt")
+                    return
+                end
             end
         end
+
+        if (save == true)
+            for i in 1:9                                                                                                                          
+                save("image-$i.jpg", images[i])                                                                                                   
+            end
+        end
+
+        @info "Images:" length(images) 
+    finally
+        close(cam)
     end
+    
+    return images, corners
 end
 
 
